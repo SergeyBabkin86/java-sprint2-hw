2290e4b2bb997074d61b27f26f13f7d9e961a877
Sergey Babkin <sergeybabkin1@gmail.com>
2022-03-06 04:47:46 +0300

    fix: reviewer's commment has been taken into account

--- src/Main.java
+++ src/Main.java
@@ -81,64 +81,11 @@ public class Main {
         for (Task historyTask : manager.history()) {
             System.out.println(historyTask);
         }
-        System.out.println("\nУдаление эпика с тремя подзадачами (удаляем эпик №2)");
+        System.out.println("\nУдаление эпика с тремя подзадачами (удаляем эпик № 2)");
         manager.deleteEpic(2);
 
         for (Task historyTask : manager.history()) {
             System.out.println(historyTask);
         }
-
-
-
-/* TEST CODE_OLD
-        System.out.println("\n2.1 Проверяем работу метода getAllTasksList().");
-        for (Object object : manager.getAllTasksList()) {
-            System.out.println(object);
-        }
-
-        System.out.println("\n2.3 Проверяем получение по идентификатору.");
-        System.out.println(manager.getEpic(2));
-        System.out.println(manager.getTask(7));
-        System.out.println(manager.getSubtask(6));
-
-        System.out.println("\n2.5. Обновление. Изменяем задачу7, эпик3, сабтаск5 и проверяем внедерение изменений");
-        Task revisedTask = new Task(7, "НоваяЗадача7", "НовоеОписание7", TaskStatus.DONE);
-        manager.updateTask(revisedTask);
-        System.out.println(manager.getTask(7));
-        Epic revisedEpic = new Epic(2, "НовыйЭпик3", "НовоеЭпическоеОписание3", null);
-        manager.updateEpic(revisedEpic);
-        System.out.println(manager.getEpic(3));
-        Subtask revisedSubtask = new Subtask(5, 2, "НовоеНазвание5", "НовоеОписание5", TaskStatus.DONE);
-        manager.updateSubtask(revisedSubtask);
-        System.out.println(manager.getEpic(2));
-        System.out.println(manager.getEpicsSubtasks(2));
-
-        System.out.println("\nИСТОРИЯ ДО УДАЛЕНИЯ:");
-
-
-        System.out.println();
-        for (Task historyTask : manager.history()) {
-            System.out.println(historyTask);
-        }
-
-        System.out.println("\n2.6. Удаление. Удаляем эпик 3, задачу 7 и подзадачу 4 (для проверки изменения статуса)");
-        manager.deleteEpic(3);
-        manager.deleteSubtask(4);
-        manager.deleteTask(7);
-
-        // Проверяем работу по удалению
-        for (Object object : manager.getAllTasksList()) {
-            System.out.println(object);
-        }
-
-        System.out.println("\nИСТОРИЯ ПОСЛЕ УДАЛЕНИЯ:");
-
-        System.out.println();
-        for (Task historyTask : manager.history()) {
-            System.out.println(historyTask);
-        }
-
-        manager.clearTaskLists();
-        */
     }
 }
